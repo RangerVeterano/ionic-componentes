9f2ba8519d698afc033fb8765ae4278e45a98f1c
Ignacio Bohigues <harvestveterano@gmail.com>
2022-02-10 13:17:38 +0100

    Fin de la seccion 5

---
+++ src/app/pages/toast/toast.page.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-toast',
+  templateUrl: './toast.page.html',
+  styleUrls: ['./toast.page.scss'],
+})
+export class ToastPage implements OnInit {
+
+  constructor(private tc: ToastController) { }
+
+  ngOnInit() {
+  }
+
+  onClick1(){
+    this.presentToast();
+  }
+
+  onClick2(){
+    this.presentToastWithOptions();
+  }
+
+  async presentToast() {
+    const toast = await this.tc.create({
+      message: 'Your settings have been saved.',
+      duration: 2000
+    });
+    toast.present();
+  }
+
+  async presentToastWithOptions() {
+    const toast = await this.tc.create({
+      header: 'Toast header',
+      message: 'Click to Close',
+      icon: 'information-circle',
+      position: 'bottom',
+      buttons: [
+        {
+          side: 'start',
+          icon: 'star',
+          text: 'Favorite',
+          handler: () => {
+            console.log('Favorite clicked');
+          }
+        }, {
+          text: 'Done',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    await toast.present();
+
+    const { role } = await toast.onDidDismiss();
+    console.log('onDidDismiss resolved with role', role);
+  }
+
+}
